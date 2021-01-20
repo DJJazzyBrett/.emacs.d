71c7858722d33681e1f8763cdd075c545616d196
DJJazzyBrett <mmoc.liamg.ta@gmail.com>
2021-01-20 18:53:19 -0500

    updt java qk

--- lisp/init-java-quickstart.el
+++ lisp/init-java-quickstart.el
@@ -4,28 +4,48 @@
 
 ;;; Code:
 
-(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :config (setq lsp-completion-enable-additional-text-edit nil))
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (java-mode . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config (setq lsp-completion-enable-additional-text-edit nil)
+  ;; Performance tweaks, see
+  ;; https://github.com/emacs-lsp/lsp-mode#performance
+  ;; (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024))
+  (setq lsp-idle-delay 0.500)
+  :commands lsp lsp-deferred)
 
 
-(use-package lsp-ui)
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
 
 
-(use-package lsp-java
-  :config
-  (add-hook 'java-mode-hook 'lsp))
+;; if you are helm user
+;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
 
 
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+
+;; optionally if you want to use debugger
 (use-package dap-mode
   :after lsp-mode
   :config (dap-auto-configure-mode))
-
 
 (use-package dap-java
   :ensure nil)
 
 
-(use-package lsp-treemacs)
+(use-package lsp-java
+  :config
+  (add-hook 'java-mode-hook 'lsp))
 
 
 (provide 'init-java-quickstart)
